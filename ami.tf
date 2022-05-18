af7275392f3368f281106367dd9513de1644e66f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-18 07:12:09 +0530

    Update terraform/tf-module-immutable-ami/

--- ami.tf
+++ ami.tf
@@ -1,5 +1,10 @@
 resource "aws_ami_from_instance" "ami" {
   name               = "${var.COMPONENT}-${var.APP_VERSION}"
   source_instance_id = aws_instance.ami-instance.id
+  tags = {
+    Name        = "${var.COMPONENT}-${var.APP_VERSION}"
+    COMPONENT   = var.COMPONENT
+    APP_VERSION = var.APP_VERSION
+  }
 }
 
